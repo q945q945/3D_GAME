14f28296e83c63e30e88c1533e66ad72301ad67c
q945q945 <71687772+q945q945@users.noreply.github.com>
2020-12-15 15:55:35 +0800

    1215_3D

---
+++ 3D_GAME_PRACTICE/Assets/Scripts/NPCDate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NPC資料", menuName ="zhen/NPC 資料")]
+public class NPCDate : ScriptableObject
+{
+    [Header("第一段對話"),TextArea]
+    public string dialougA;
+    [Header("第二段對話")]
+    public string dialougB;
+    [Header("第三段對話")]
+    public string dialougC;
+    [Header("任務項目需求")]
+    public string count;
+    [Header("已取得項目數量")]
+    public string countCurrent;
+}
